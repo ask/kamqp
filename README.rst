5ced09ea8b3254a4df6b715feed13120731fd705
Ask Solem <ask@celeryproject.org>
2011-11-09 16:31:29 +0000

    Initial import from amqplib 1.0.2

---
+++ README.rst
@@ -0,0 +1,8 @@
+kAMQP is a fork of `py-amqplib`_ with improvements and experimental features.
+
+It is the main driver for the `Kombu`_ library.
+
+The original author is Barry Pederson, maintained by Ask Solem.
+
+.. _`py-amqplib`: http://barryp.org/software/py-amqplib/
+.. _`Kombu`: http://kombu.me/
---
+++ kamqp/__init__.py
@@ -0,0 +1,13 @@
+"""amqp driver for Kombu (amqplib fork)"""
+# :copyright: (c) Barry Pederson
+# :license:   LGPL, see LICENSE for more details.
+
+VERSION = (0, 0, 1)
+
+__version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
+__author__ = "Barry Pederson, Ask Solem"
+__contact__ = "ask@celeryproject.org"
+__homepage__ = "http://celeryproject.org"
+__docformat__ = "restructuredtext"
+
+# -eof meta-
